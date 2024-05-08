b16a28a72c2ed6f91e32484c4238454bb9af0bea
victorhugotagliapietra <vhtorugo1996@gmail.com>
2024-05-08 15:10:27 -0300

    feat: create base repository

--- bry-challenge/src/main/java/com/brytask/repository/UserRepository.java
+++ bry-challenge/src/main/java/com/brytask/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.brytask.repository;
 
-public class UserRepository {
+import com.brytask.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
