5ce72f20661c27a0071e7cf9ba3f9d5bd962c2ad
victorhugotagliapietra <vhtorugo1996@gmail.com>
2024-05-08 16:04:03 -0300

    feat: create User service

--- bry-challenge/src/main/java/com/brytask/service/UserService.java
+++ bry-challenge/src/main/java/com/brytask/service/UserService.java
@@ -1,5 +1,37 @@
 package com.brytask.service;
 
+import com.brytask.model.User;
+import com.brytask.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  // Search for all Users
+  public List<User> findAllUsers() {
+    return userRepository.findAll();
+  }
+
+  // Search an User by ID
+  public Optional<User> findUserById(Long id) {
+    return userRepository.findById(id);
+  }
+
+  // Save or Update User
+  public User saveOrUpdateUser(User user) {
+    return userRepository.save(user);
+  }
+
+  // Delete User
+  public void deleteUser(Long id) {
+    userRepository.deleteById(id);
+  }
 
 }
