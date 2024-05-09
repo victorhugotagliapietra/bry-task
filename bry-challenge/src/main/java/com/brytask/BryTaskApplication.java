5187832bbf53d652ec974fff8cd6969d432cc15d
victorhugotagliapietra <vhtorugo1996@gmail.com>
2024-05-09 14:30:39 -0300

    feat: create project application file

---
+++ bry-challenge/src/main/java/com/brytask/BryTaskApplication.java
@@ -0,0 +1,12 @@
+package com.brytask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BryTaskApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BryTaskApplication.class, args);
+  }
+}
